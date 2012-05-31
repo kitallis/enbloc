2c576b603c2ad1cef7d4f2a7e3c652d76f1e9317
Akshay Gupta <akshay@c42.in>
2012-05-31 21:35:26 +0530

    we have a caption class that does the heavy lifting

--- enbloc.js
+++ enbloc.js
@@ -1,8 +1,8 @@
 Video = function(video) {
-	this.video = video;
+	this.video = $(video);
 };
 
-Video.prototype {
+Video.prototype = {
 	height: function() {
 		return this.video.height();
 	},
@@ -12,19 +12,19 @@ Video.prototype {
 	},
 	
 	top: function() {
-		return this.video.left();
+		return this.video.position().top;
 	},
 	
 	right: function() {
-		return this.video.right();
+		return this.video.position().right;
 	},
 
 	bottom: function() {
-		return this.video.bottom();
+		return this.video.position().bottom;
 	},
 
 	left: function() {
-		return this.video.left();
+		return this.video.position().left;
 	},
 
 	lowerThirdHeight: function() {
@@ -44,75 +44,157 @@ Video.prototype {
 	}
 };
 
-Caption = function(element, video) {
-	this.element = element;
-	this.video = video;
-	this.initialize();
+EnBlocPopcorn = function(popcorn, caption) {
+	this.popcorn = popcorn;
+	this.caption = caption;
 };
 
-Caption.prototype {
-	initialize: function() {}
-}
-
-EnBlocPopcorn = function(video, element, html, start, end) {
-	this.popcorn = Popcorn(video)
-	this.caption = new Caption(element, html);
-
-	this.start = start;
-	this.end = end;
-};
-
-EnBlocPopcorn.prototype {
+EnBlocPopcorn.prototype = {
 	bindCaptionWithTime: function() {
-		var self = this;
-		this.popcorn.listen('timeupdate', function() { self.showOrHideElement(); });
-	},
-
-	showOrHideElement: function() {
-		if (this.currentTime() === this.start) {
-			element.html(html);
-		} else {
-			element.empty();
-		}
-
+    var self = this;
+		this.popcorn.on('timeupdate', function() {
+      self.caption.showOrHide(Math.round(this.currentTime())); 
+    });   
 	}
 };
 
 EnBloc = function(video) {
 	this.video = new Video(video);
+  this.popcorn = new Popcorn(video);
 };
 
-EnBloc.prototype {
+EnBloc.prototype = {
 	caption: function() {
-		for (var caption in arguments) {
-			this.initialize(caption);
+		for (var caption = 0; caption < arguments.length; caption++) {
+			this.initialize(arguments[caption]);
 		};
 	},
 
 	initialize: function(options) {
-		this.start = this.setNumericOptions(options.start);
-		this.end = this.setNumericOptions(options.end);
+		var start = this.setNumericOptions(options.start);
+		var end = this.setNumericOptions(options.end);
 
-		this.fadeIn = this.setNumericOptions(options.fadeIn);
-		this.fadeOut = this.setNumericOptions(options.fadeOut);
+		var fadeIn = this.setNumericOptions(options.fadeIn);
+		var fadeOut = this.setNumericOptions(options.fadeOut);
 
-		this.enBlocPopcorn = new EnBlocPopcorn(this.video.video, this.start, this.end, this.fadeIn, this.fadeOut);
-		this.enBlocPopcorn.bindCaptionWithTime();
+		var position = (options.position || "footer");
 
-		this.position = options.position || "footer";
-		this.element = this.textOrHTML(options.element);
+		var element = options.element;
+
+    var caption = new Caption(this.video, 
+                              element, 
+                              position, 
+                              start, 
+                              end, 
+                              fadeIn, 
+                              fadeOut);
+		var enBlocPopcorn = new EnBlocPopcorn(this.popcorn, caption);
+		enBlocPopcorn.bindCaptionWithTime();
 	},
 
 	setNumericOptions: function(option) {
-		option || 0;
+		return (option || 0);
 	},
 
-	textOrHTML: function(element) {
-		if ($(element).length) {
-			element.hide(); // don't think this is a good place to put this.
-			return element.html();
+  play: function() {
+    this.popcorn.play();
+  }
+};
+
+Caption = function(video, element, position, start, end, fadeIn, fadeOut) {
+  this.element = element;
+  this.position = position;
+  this.video = video;
+
+  this.start = start;
+  this.end = end;
+
+  this.fadeIn = fadeIn;
+  this.fadeOut = fadeOut;
+
+	this.initialize();
+};
+
+Caption.prototype = {
+	initialize: function() {
+    var captionDiv = '<div class="caption"></div>';
+    this.enBlocCaption = this.append($(document.body), captionDiv);
+
+    this.setEnBlocCaptionStyles();
+    this.insertElement();
+  },
+
+  append: function(body, captionDiv) {
+    body.append(captionDiv);
+    return $("." + captionDiv);
+  },
+
+  setEnBlocCaptionStyles: function() {
+    var self = this;
+
+    this.enBlocCaption.css('top', function() { return self.setTop(); });
+    //this.enBlocCaption.css('left', function() { self.setLeft(); });
+
+    if (!this.isHTML(this.element)) {
+      this.enBlocCaption.css('background', 'green');
+      this.enBlocCaption.css('z-index', 5000);
+      this.enBlocCaption.css('position', 'absolute');
+      this.enBlocCaption.css('color', '#eee');
+      this.enBlocCaption.css('width', '100px');
+      this.enBlocCaption.css('display', 'block');
+    }
+  },
+
+  setTop: function() {
+    if (this.position === "full") {
+      console.log(this.video.top());
+      return this.video.top();
+    } else {
+      return this.video.lowerThirdHeight();
+    }
+  },
+
+  setLeft: function() {
+    if (this.position === "full") {
+      return this.video.left();
+    } else {
+      return this.video.lowerThirdWidth();
+    }
+  },
+
+  insertElement: function() {
+    var element = this.HTMLOrText(this.element);
+    this.enBlocCaption.html(element);
+    
+    //this.enBlocCaption.hide();
+  },
+
+  // it'll either be a custom DOM element or just plain text.
+	HTMLOrText: function(element) {
+    if (this.isHTML(element)) {
+      //$(element).hide();
+			return $(element).html();
 		} else {
-			return element.text();
+			return element;
+		}
+	},
+
+  isHTML: function(element) {
+		var DOMElement = $(element);
+
+    if (DOMElement.length) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+	showOrHide: function(currentTime) {
+    if (currentTime === this.start) {
+			this.enBlocCaption.show();
+      console.log(this.enBlocCaption);
+		} else if (currentTime == this.end) {
+      //this.enBlocCaption.hide();
 		}
 	}
 };
